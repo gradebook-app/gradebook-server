9b3935240d302e117cbe9aef2415de2fdf6d3a7e
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-150.local>
2021-11-01 16:55:45 -0400

    Updated Architecture

--- app.py
+++ app.py
@@ -9,23 +9,21 @@ def init_app():
 
     mongodb_client.init_app(app)
 
+    if __name__ == "__main__":
+        from modules.auth.auth_controller import auth as auth_blueprint 
+        from modules.grades.grades_controller import grades as grades_blueprint 
+        
+        app.register_blueprint(auth_blueprint)
+        app.register_blueprint(grades_blueprint)
+
     return app
 
 app = init_app()
 
-def init_blueprints(): 
-    from modules.auth.auth_controller import auth as auth_blueprint 
-    from modules.grades.grades_controller import grades as grades_blueprint 
-    
-    app.register_blueprint(auth_blueprint)
-    app.register_blueprint(grades_blueprint)
+@app.route('/', methods=['GET'])
+def home(): 
+    return "All Systems Operational."
 
 if __name__ == "__main__": 
-    init_blueprints()
-
-    @app.route('/', methods=['GET'])
-    def home(): 
-        return "All Systems Operational."
-
     app.run(port=5000, debug=True)
 
