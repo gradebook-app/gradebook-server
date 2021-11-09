795cc8f3fa37f0da3c56451563b27a113a8e927f
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-150.local>
2021-11-08 20:01:39 -0500

    Updated FCM Service

--- modules/firebase/fcm_service.py
+++ modules/firebase/fcm_service.py
@@ -1,11 +1,9 @@
 from pyfcm import FCMNotification
-from twisted.internet import reactor
 from config.config import config
 
 class FCMService: 
     def __init__(self): 
         self.push_service = FCMNotification(api_key=config['fcm']['key'])
-        reactor.run()
 
     def send_message(self, token, title, message): 
         self.push_service.notify_single_device(
