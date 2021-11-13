9a2097c90b60061111ec6bdfa35fa3d3c58f36c1
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-150.local>
2021-11-13 10:50:14 -0500

    Change Mongo URI Config

--- config/env/production.py
+++ config/env/production.py
@@ -2,7 +2,7 @@ from decouple import config
 
 config = {
     "mongodb": {
-        "uri": "mongodb+srv://admin:Rq9hS8woGso0w5KD@gradebook.nwijt.mongodb.net/gradebook?authSource=admin&replicaSet=atlas-293qt9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+        "uri": config('MONGO_URI'),
         "db": "gradebook"
     },
      "jwt": {
