609105b51291d1a6841c69b64419dd392ab214e6
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-212.local>
2021-12-02 01:29:35 -0500

    Clear Queue

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from modules.auth.auth_controller import auth as auth_blueprint 
 from modules.grades.grades_controller import grades as grades_blueprint 
 from modules.user.user_controller import user as user_blueprint
+from modules.redis.grades import clear_queue
 
 app = Flask(__name__, instance_relative_config=False)
+
+clear_queue()
 
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(grades_blueprint)
